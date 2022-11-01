4a0946c65d22144135cc67937e09912cd6d48d66
CCDM06 <ccdm06@CCDM06.local>
2022-11-01 15:51:36 -0700

    Primer JavaScript

---
+++ primerjavascript.js
@@ -0,0 +1,19 @@
+function quitarSITEC(){
+    // let pregunta = confirm("Seguro de quitar SITEC?");
+    if(confirm("Seguro de quitar SITEC?")){
+        alert("SITEC Borrado ... :(");
+        let chi = document.getElementById("sitec");
+        chi.innerHTML = "shiiiaaaale";
+
+    }
+}
+
+function funcionAlerta(){
+    alert("di clic");
+}
+
+
+// alert("Alerta en archivo externo javaScript");
+/*
+ Varias lineas de comentarios
+*/
